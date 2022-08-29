13d78f9e81a02914e69b4d7762460282b8c934f3
maklakovsb <maklakovsb@yandex.ru>
2022-08-29 15:12:39 +0300

    Добавлены делегаты команд.

---
+++ RailwayPark/Delegates/DelegateParametrizedCommand.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace RailwayPark.Delegates
+{
+    public class DelegateParametrizedCommand : ICommand
+    {
+        private readonly Predicate<object> _canExecute;
+        private readonly Action<object> _execute;
+
+        public event EventHandler CanExecuteChanged;
+
+        public DelegateParametrizedCommand(Action<object> execute)
+            : this(execute, null)
+        {
+        }
+
+        public DelegateParametrizedCommand(Action<object> execute, Predicate<object> canExecute)
+        {
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute == null || _canExecute(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute(parameter);
+        }
+
+        public void RaiseCanExecuteChanged()
+        {
+            CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
