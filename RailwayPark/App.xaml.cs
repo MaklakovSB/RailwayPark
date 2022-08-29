ad366c7d0d93e860e2a4c930eac07627ea910fca
maklakovsb <maklakovsb@yandex.ru>
2022-08-29 15:26:15 +0300

    Удалена точка входа из разметки App и добавлена точка входа перегрузкой метода OnStartup в коде App.

--- RailwayPark/App.xaml.cs
+++ RailwayPark/App.xaml.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using RailwayPark.ViewModels;
+using RailwayPark.Views;
 using System.Windows;
 
 namespace RailwayPark
@@ -13,5 +9,16 @@ namespace RailwayPark
     /// </summary>
     public partial class App : Application
     {
+        /// <summary>
+        /// Точкка входа.
+        /// </summary>
+        /// <param name="e"></param>
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            var mv = new MainView(new MainViewModel());
+            mv.Show();
+        }
     }
 }
