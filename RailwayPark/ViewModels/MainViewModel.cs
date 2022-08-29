9ac0c13a4234c73ae2e6e931d650b7822c1681a9
maklakovsb <maklakovsb@yandex.ru>
2022-08-29 15:22:49 +0300

    Удалено основное окно, добавлено основное представление и модель представления. Изменение файла проекта в связи удалением и добавлением файлов в проект.

---
+++ RailwayPark/ViewModels/MainViewModel.cs
@@ -0,0 +1,28 @@
+﻿using RailwayPark.Interfaces;
+using System;
+
+namespace RailwayPark.ViewModels
+{
+    public class MainViewModel : IViewModel
+    {
+
+        /// <summary>
+        /// Конструктор.
+        /// </summary>
+        public MainViewModel()
+        {
+
+        }
+
+        #region Имплементация IViewModel
+
+        public event Action Close;
+
+        public virtual void OnClose()
+        {
+            Close?.Invoke();
+        }
+
+        #endregion
+    }
+}
---
+++ RailwayPark/Views/MainView.xaml.cs
@@ -0,0 +1,31 @@
+﻿using RailwayPark.Interfaces;
+using System.Windows;
+
+namespace RailwayPark.Views
+{
+    /// <summary>
+    /// Логика взаимодействия для MainView.xaml
+    /// </summary>
+    public partial class MainView : Window, IView
+    {
+        /// <summary>
+        /// Конструктор.
+        /// </summary>
+        /// <param name="viewModel"></param>
+        public MainView(IViewModel viewModel)
+        {
+            ViewModel = viewModel;
+            InitializeComponent();
+        }
+
+        #region Имплементация IView
+
+        public IViewModel ViewModel
+        {
+            get { return DataContext as IViewModel; }
+            set { DataContext = value; }
+        }
+
+        #endregion
+    }
+}
