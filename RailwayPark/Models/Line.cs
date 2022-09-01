55d55efc7b62857566eb0e83840e8e0299980157
maklakovsb <maklakovsb@yandex.ru>
2022-09-01 04:21:19 +0300

    Добавлена модель пути.

---
+++ RailwayPark/Models/Line.cs
@@ -0,0 +1,31 @@
+﻿using RailwayPark.Models;
+using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Media;
+
+namespace RailwayPark.ViewModels
+{
+    public class Line : BasePrimitive
+    {
+        /// <summary>
+        /// Коллекция Points.
+        /// </summary>
+        public ObservableCollection<Point> Points
+        {
+            get { return points; }
+        }
+        private ObservableCollection<Point> points = new ObservableCollection<Point>();
+
+        /// <summary>
+        /// Цвет заливки.
+        /// </summary>
+        public Color Fill
+        {
+            get { return fill; }
+            set { fill = value; }
+        }
+        private Color fill = Brushes.Transparent.Color;
+
+        public override string PrimitiveType => "Line";
+    }
+}
