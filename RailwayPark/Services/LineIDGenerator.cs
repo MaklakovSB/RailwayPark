e55eb093ff18df9299e85d8807e653795c0b9e9d
maklakovsb <maklakovsb@yandex.ru>
2022-09-02 00:23:50 +0300

    Добавлены генераторы идентификаторов для объектов Line и Vertex.

---
+++ RailwayPark/Services/LineIDGenerator.cs
@@ -0,0 +1,35 @@
+﻿namespace RailwayPark.Services
+{
+    /// <summary>
+    /// Генератор идентификаторов для объектов Line.
+    /// </summary>
+    public sealed class LineIDGenerator
+    {
+        /// <summary>
+        /// Явный статический конструктор.
+        /// </summary>
+        static LineIDGenerator() { }
+
+        /// <summary>
+        /// Закрытый конструктор.
+        /// </summary>
+        private LineIDGenerator() { }
+
+        private uint nextID = 1;
+
+        private static readonly LineIDGenerator source = new LineIDGenerator();
+
+        public static LineIDGenerator Source
+        {
+            get
+            {
+                return source;
+            }
+        }
+
+        public uint nextId()
+        {
+            return nextID++;
+        }
+    }
+}
---
+++ RailwayPark/Services/VertexIDGenerator.cs
@@ -0,0 +1,35 @@
+﻿namespace RailwayPark.Services
+{
+    /// <summary>
+    /// Генератор идентификаторов для объектов Vertex.
+    /// </summary>
+    public sealed class VertexIDGenerator
+    {
+        /// <summary>
+        /// Явный статический конструктор.
+        /// </summary>
+        static VertexIDGenerator() { }
+
+        /// <summary>
+        /// Закрытый конструктор.
+        /// </summary>
+        private VertexIDGenerator() { }
+
+        private uint nextID = 1;
+
+        private static readonly VertexIDGenerator source = new VertexIDGenerator();
+
+        public static VertexIDGenerator Source
+        {
+            get
+            {
+                return source;
+            }
+        }
+
+        public uint nextId()
+        {
+            return nextID++;
+        }
+    }
+}
