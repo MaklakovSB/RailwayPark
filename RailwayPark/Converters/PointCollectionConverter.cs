5f7c06a3ba9594ef4db5542215218b9b7b104fc9
maklakovsb <maklakovsb@yandex.ru>
2022-09-01 04:07:54 +0300

    Добавлены конвертеры.

---
+++ RailwayPark/Converters/PointCollectionConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace RailwayPark.Converters
+{
+    public class PointCollectionConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value.GetType() == typeof(ObservableCollection<Point>) && targetType == typeof(PointCollection))
+            {
+                var pointCollection = new PointCollection();
+
+                foreach (var point in value as ObservableCollection<Point>)
+                {
+                    pointCollection.Add(point);
+                }
+
+                return pointCollection;
+            }
+
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException("ConvertBack not supported");
+        }
+    }
+}
---
+++ RailwayPark/Converters/VertexPositionConverter.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace RailwayPark.Converters
+{
+    public class VertexPositionConverter : IMultiValueConverter
+    {
+        public object Convert(object[] value, Type targetType, object parameter, CultureInfo culture)
+        {
+           
+            if (value.GetType() == typeof(object[]))
+            {
+                if (targetType == typeof(double))
+                {
+                    if (value.Length == 2)
+                    {
+                        if(value[0].GetType() == typeof(double) && value[1].GetType() == typeof(string))
+                        {
+                            var Value = (double)value[0];
+
+                            if ((string)value[1] == "Vertex")
+                            {
+                                return Value - 1;
+                            }
+                            else
+                            {
+                                return Value;
+                            }
+                        }
+                        else { throw new ArgumentException("Не правильные типы значений внутри мвссива value"); }
+                    }
+                    else { throw new ArgumentException("Не правильное значение Length аргумента value"); }
+                }
+                else { throw new ArgumentException("Не правильный тип аргумента targetType"); }
+            }
+            else { throw new ArgumentException("Не правильный тип аргумента value"); }           
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException("Метод обратной конвертации не поддерживается");
+        }
+    }
+}
