56499759eb831774397a3e6f87c5c644a31877a9
maklakovsb <maklakovsb@yandex.ru>
2022-09-01 04:09:51 +0300

    Добавлена модель базового примитива.

---
+++ RailwayPark/Models/BasePrimitive.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace RailwayPark.Models
+{
+    public abstract class BasePrimitive
+    {
+        public Double X { get; set; }
+        public Double Y { get; set; }
+        public int Z { get; set; }
+        public Double Height { get; set; }
+        public Double Width { get; set; }
+
+        public abstract string PrimitiveType 
+        {
+            get; 
+        }
+    }
+}
