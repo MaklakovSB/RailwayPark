f935f162c6903973b01faf6ec4fbcb197940615c
maklakovsb <maklakovsb@yandex.ru>
2022-09-01 15:17:55 +0300

    Добавлен id к базовому примитиву.

--- RailwayPark/Models/BasePrimitive.cs
+++ RailwayPark/Models/BasePrimitive.cs
@@ -4,11 +4,12 @@ namespace RailwayPark.Models
 {
     public abstract class BasePrimitive
     {
-        public Double X { get; set; }
-        public Double Y { get; set; }
-        public int Z { get; set; }
-        public Double Height { get; set; }
-        public Double Width { get; set; }
+        public uint Id { set; get; }
+        public Double X { set; get; }
+        public Double Y { set; get; }
+        public int Z { set; get; }
+        public Double Height { set; get; }
+        public Double Width { set; get; }
 
         public abstract string PrimitiveType 
         {
