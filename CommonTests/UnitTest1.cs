ee7a1c85911b5e6b1c62664b40fe080c9157a3fd
maklakovsb <maklakovsb@yandex.ru>
2022-09-03 02:39:30 +0300

    Добавлен проект для автотестов решения. Добавлен тест для проверки библиотеки графов.

--- CommonTests/UnitTest1.cs
+++ CommonTests/UnitTest1.cs
@@ -1,3 +1,4 @@
+using Graph;
 using NUnit.Framework;
 
 namespace CommonTests
@@ -10,9 +11,37 @@ namespace CommonTests
         }
 
         [Test]
-        public void Test1()
+        public void TestGraph()
         {
-            Assert.Pass();
+            GraphDFS graph = new GraphDFS();
+
+            graph.Verteces.Add(new GraphVertex(0, 0));
+            graph.Verteces.Add(new GraphVertex(1, 0));
+            graph.Verteces.Add(new GraphVertex(2, 2));
+            graph.Verteces.Add(new GraphVertex(0, 2));
+
+            graph.Verteces.Add(new GraphVertex(2, 0));
+
+            graph.Edges.Add(new GraphEdge(0, 1));
+            graph.Edges.Add(new GraphEdge(1, 2));
+            graph.Edges.Add(new GraphEdge(2, 3));
+            graph.Edges.Add(new GraphEdge(3, 0));
+
+            graph.Edges.Add(new GraphEdge(1, 4));
+            graph.Edges.Add(new GraphEdge(4, 2));
+
+            graph.CyclesSearch();
+
+            var test = graph.Cycles;
+
+            if(test.Count == 3)
+            {
+                Assert.Pass();
+            }
+            else
+            {
+                Assert.Fail();
+            }       
         }
     }
 }
