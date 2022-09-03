eb75db78fc3ddb068b15a0d8a716ffb041e68fb6
maklakovsb <maklakovsb@yandex.ru>
2022-09-03 06:48:34 +0300

    Добавлен генератор идентификаторов для объектов Area.

---
+++ RailwayPark/Services/AreaIDGenerator.cs
@@ -0,0 +1,32 @@
+﻿namespace RailwayPark.Services
+{
+    public sealed class AreaIDGenerator
+    {
+        /// <summary>
+        /// Явный статический конструктор.
+        /// </summary>
+        static AreaIDGenerator() { }
+
+        /// <summary>
+        /// Закрытый конструктор.
+        /// </summary>
+        private AreaIDGenerator() { }
+
+        private uint nextID = 1;
+
+        private static readonly AreaIDGenerator source = new AreaIDGenerator();
+
+        public static AreaIDGenerator Source
+        {
+            get
+            {
+                return source;
+            }
+        }
+
+        public uint nextId()
+        {
+            return nextID++;
+        }
+    }
+}
