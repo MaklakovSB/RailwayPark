cd19f0706204466035a0d480ef24e867519e559a
maklakovsb <maklakovsb@yandex.ru>
2022-09-03 06:41:14 +0300

    Добавлен класс Area для замкнутых линий, чтобы заливать области.

---
+++ RailwayPark/Models/Area.cs
@@ -0,0 +1,34 @@
+﻿using RailwayPark.Models;
+using RailwayPark.Services;
+using System.Collections.ObjectModel;
+using System.Windows;
+
+namespace RailwayPark.ViewModels
+{
+    public sealed class Area : BasePrimitive
+    {
+        /// <summary>
+        /// Идентификатор линии.
+        /// </summary>
+        public uint AreaID { protected set; get; }
+
+        /// <summary>
+        /// Коллекция Points.
+        /// </summary>
+        public ObservableCollection<Point> Points
+        {
+            get { return points; }
+        }
+        private ObservableCollection<Point> points = new ObservableCollection<Point>();
+
+        /// <summary>
+        /// Конструктор.
+        /// </summary>
+        public Area()
+        {
+            AreaID = AreaIDGenerator.Source.nextId();
+        }
+
+        public override string PrimitiveType => "Area";
+    }
+}
